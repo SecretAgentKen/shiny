ea98c6ec6f562519f9ac6c3385974f20acacd005
Ken VanderVeer <gtaluvit@gmail.com>
2022-06-14 23:03:31 -0400

    Update to manifest 3

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,136 @@
 <template>
 	<nav class="panel">
-		<p class="panel-heading">Ignore/Retain the following sites for cookies, cache, and storage</p>
+		<p class="panel-heading">
+			Ignore/Retain the following sites for cookies, cache, and storage
+		</p>
 		<div class="panel-block">
 			<div class="buttons">
 				<button class="button" @click.prevent="selectAll()">
 					Select All
 				</button>
-				<button class="button" :disabled="!hasSelected" @click.prevent="removeOrigins()">
+				<button
+					class="button"
+					:disabled="!hasSelected"
+					@click.prevent="removeOrigins()"
+				>
 					Remove Selected
 				</button>
 			</div>
-			{{validOrigin}}
 		</div>
 		<div class="panel-block">
 			<div class="field is-grouped is-expanded">
 				<div class="control is-expanded">
-					<input class="input" type="text" placeholder="https://example.com" v-model="inputOrigin" @keyup.enter="addOrigin()">
+					<input
+						class="input"
+						type="text"
+						placeholder="https://example.com"
+						v-model="inputOrigin"
+						@keyup.enter="addOrigin()"
+					/>
 				</div>
 				<div class="control">
-					<button class="button is-primary" :disabled="!canAdd" @click="addOrigin()">Add URL</button>
+					<button
+						class="button is-primary"
+						:disabled="!canAdd"
+						@click="addOrigin()"
+					>
+						Add URL
+					</button>
 				</div>
 			</div>
 		</div>
 		<label class="panel-block" v-for="obj in originObjects">
-			<input type="checkbox" v-model="obj.selected">{{obj.origin}}
+			<input type="checkbox" v-model="obj.selected" />{{ obj.origin }}
 		</label>
 	</nav>
 </template>
 
 <script>
 export default {
-	data(){
+	data() {
 		return {
 			originObjects: [],
-			inputOrigin: ''
-		}
+			inputOrigin: "",
+		};
 	},
-	mounted(){
+	mounted() {
 		this.loadOrigins();
 	},
-	computed:{
-		hasSelected(){
-			return !!this.originObjects.find((obj)=>{
+	computed: {
+		hasSelected() {
+			return !!this.originObjects.find((obj) => {
 				return obj.selected;
 			});
 		},
-		canAdd(){
-			if ( !this.inputOrigin ) return false;
+		canAdd() {
+			if (!this.inputOrigin) return false;
 			try {
 				return !!new URL(this.inputOrigin).origin;
-			} catch(e){
+			} catch (e) {
 				return false;
 			}
 		},
-		validOrigin(){
+		validOrigin() {
 			try {
 				return new URL(this.inputOrigin).origin;
-			} catch(e){
-				return '';
+			} catch (e) {
+				return "";
 			}
-		}
+		},
 	},
 	methods: {
-		selectAll(){
+		selectAll() {
 			let setting = null;
-			this.originObjects.forEach((obj)=>{
-				if ( setting === null ) {
+			this.originObjects.forEach((obj) => {
+				if (setting === null) {
 					setting = !obj.selected;
 				}
 				obj.selected = setting;
 			});
 		},
-		addOrigin(){
-			if ( this.canAdd ) {
-				let data = JSON.parse(localStorage['excludes']);
-  				let origin = this.validOrigin;
-  				if ( data.excludes.indexOf(origin) < 0 ) {
-    				data.excludes.push(origin);
-					data.excludes.sort();
-					localStorage['excludes'] = JSON.stringify(data);
-				}
-				this.inputOrigin = '';
-				this.loadOrigins();
+		addOrigin() {
+			if (this.canAdd) {
+				chrome.storage.sync.get("excludes", (data) => {
+					let origin = this.validOrigin;
+					if (data.excludes.indexOf(origin) < 0) {
+						data.excludes.push(origin);
+						data.excludes.sort();
+						chrome.storage.sync.set({ excludes: data.excludes });
+					}
+					this.inputOrigin = "";
+					this.loadOrigins();
+				});
 			}
 		},
-		loadOrigins(){
-			this.originObjects = JSON.parse(localStorage['excludes']).excludes.map((origin)=>{
-				return {origin, selected: false};	
+		loadOrigins() {
+			chrome.storage.sync.get("excludes", (data) => {
+				this.originObjects = data.excludes.map((origin) => {
+					return { origin, selected: false };
+				});
 			});
 		},
-		removeOrigins(){
-			let data = {excludes:[]};
-			data.excludes = this.originObjects.filter((obj)=>{ return !obj.selected }).map((obj)=>{return obj.origin});
+		removeOrigins() {
+			let data = { excludes: [] };
+			data.excludes = this.originObjects
+				.filter((obj) => {
+					return !obj.selected;
+				})
+				.map((obj) => {
+					return obj.origin;
+				});
 			data.excludes.sort();
-			localStorage['excludes'] = JSON.stringify(data);
+			chrome.storage.sync.set({ excludes: data.excludes });
 			this.loadOrigins();
-		}
-	}
-}
+		},
+	},
+};
 </script>
 
 <style lang="scss">
-	nav {
-		margin: 2rem;
-	}
-	.field {
-		flex-grow: 1;
-	}
+nav {
+	margin: 2rem;
+}
+.field {
+	flex-grow: 1;
+}
 </style>
--- src/clean.js
+++ src/clean.js
@@ -1,75 +1,91 @@
-(function(){
-  localStorage['excludes'] = localStorage['excludes'] || JSON.stringify({excludes:[]});
+(function () {
 
-  let data = JSON.parse(localStorage['excludes']);
-
-  chrome.runtime.onStartup.addListener(function(){
-    chrome.browsingData.remove({
-      "since": 0
-    },{
-      "appcache": true,
-      "downloads": true,
-      "formData": true,
-      "passwords": true
-    });
-    chrome.browsingData.remove(
-    { 
-      "since" : 0,
-      "excludeOrigins": data.excludes
-    }, {
-      "cache": true,
-      "cacheStorage": true,
-      "cookies": true,
-      "fileSystems": true,
-      //"history": false,
-      "indexedDB": true,
-      "localStorage": true,
-      "pluginData": true,
-      "serviceWorkers": true,
-      "webSQL": true
-    });
+  chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.get(["excludes"], (data) => {
+      if (!data.excludes) {
+        chrome.storage.sync.set({ excludes: [] })
+      }
+      updateAllTabs(data.excludes || []);
+    })
   });
 
-  chrome.tabs.onUpdated.addListener(setTabAction);
-  chrome.pageAction.onClicked.addListener(pageClicked);
+  chrome.runtime.onStartup.addListener(() => {
+    chrome.storage.sync.get(["excludes"], (data) => {
+      updateAllTabs(data.excludes);
+      chrome.browsingData.remove({
+        "since": 0
+      }, {
+        "appcache": true,
+        "downloads": true,
+        "formData": true,
+        "passwords": true
+      });
+      chrome.browsingData.remove(
+        {
+          "since": 0,
+          "excludeOrigins": data.excludes
+        }, {
+        "cache": true,
+        "cacheStorage": true,
+        "cookies": true,
+        "fileSystems": true,
+        //"history": false,
+        "indexedDB": true,
+        "localStorage": true,
+        "pluginData": true,
+        "serviceWorkers": true,
+        "webSQL": true
+      });
+    })
+  });
 
-  updateAllTabs();
+  chrome.tabs.onUpdated.addListener((id, junk, tab) => {
+    chrome.storage.sync.get(["excludes"], (data) => {
+      setTabAction(id, data.excludes, tab);
+    })
+  });
 
-  function pageClicked(tab){
-    try {
-      let origin = new URL(tab.url).origin;
-      let data = JSON.parse(localStorage['excludes']);
-      if ( data.excludes.indexOf(origin) < 0 ) {
-        data.excludes.push(origin);
-        data.excludes.sort();
-      } else {
-        data.excludes = data.excludes.filter((o)=>{return o !== origin});
+  chrome.action.onClicked.addListener(tab => {
+    chrome.storage.sync.get(["excludes"], (data) => {
+      try {
+        let origin = new URL(tab.url).origin;
+        if (data.excludes.indexOf(origin) < 0) {
+          console.log('Would add')
+          data.excludes.push(origin);
+          data.excludes.sort();
+        } else {
+          console.log('Would remove')
+          data.excludes = data.excludes.filter((o) => { return o !== origin });
+        }
+        chrome.storage.sync.set({ excludes: data.excludes })
+        updateAllTabs(data.excludes || []);
+
+      } catch (e) {
+        console.error(e)
       }
-      localStorage['excludes'] = JSON.stringify(data);
-      updateAllTabs();
-    } catch(e){}
-  }
+    })
+  })
 
-  function updateAllTabs(){
-    chrome.tabs.query({}, function(tabs){
+  function updateAllTabs(excludes) {
+    chrome.tabs.query({}, function (tabs) {
       tabs.forEach(tab => {
-        setTabAction(tab.id, null, tab);
+        setTabAction(tab.id, excludes, tab);
       })
     })
   }
 
-  function setTabAction(tabId, info, tab) {
+  function setTabAction(tabId, excludes, tab) {
     try {
       let origin = new URL(tab.url).origin;
-      let data = JSON.parse(localStorage['excludes']);
-      if ( data.excludes.indexOf(origin) < 0 ) {
-        chrome.pageAction.setIcon({tabId, path: 'img/dump.png'});
-        chrome.pageAction.setTitle({tabId, title: 'Site data will be cleared at startup'});
+      if (excludes.indexOf(origin) < 0) {
+        chrome.action.setIcon({ tabId, path: 'img/dump.png' });
+        chrome.action.setTitle({ tabId, title: 'Site data will be cleared at startup' });
       } else {
-        chrome.pageAction.setIcon({tabId, path: 'img/protection.png'});
-        chrome.pageAction.setTitle({tabId, title: 'Site data will retained at startup'});
+        chrome.action.setIcon({ tabId, path: 'img/protection.png' });
+        chrome.action.setTitle({ tabId, title: 'Site data will retained at startup' });
       }
-      chrome.pageAction.show(tabId);
-    } catch(e){}
+    } catch (e) {
+      console.error(e, tab)
+    }
   }
 })();
